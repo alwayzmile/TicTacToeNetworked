983a6ed6060a80ded0f691c8664f79e774dcf303
alwayzmile <alwayz.for.zmile@gmail.com>
2015-12-04 08:36:19 +0700

    Adding current completed
    Adding the current completed of the project.

---
+++ IkhsanTicTacToeClient/src/com/ikhsan/practice/IkhsanTicTacToeClient.java
@@ -0,0 +1,40 @@
+package com.ikhsan.practice;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class IkhsanTicTacToeClient {
+    private static int PORT = 8901;
+    private Socket socket;
+    private BufferedReader in;
+    private PrintWriter out;
+
+    public IkhsanTicTacToeClient(String serverAddress) throws Exception {
+        socket = new Socket(serverAddress, PORT);
+        in = new BufferedReader(new InputStreamReader(
+            socket.getInputStream()));
+        out = new PrintWriter(socket.getOutputStream(), true);
+    }
+    
+    public void play(String command) throws Exception {
+        String response;
+        try {
+            // write the request to server
+            out.println(command);
+            // read the response from server
+            response = in.readLine();
+            System.out.println(response);
+        }
+        finally {
+            socket.close();
+        }
+    }
+    
+    public static void main(String[] args) throws Exception {
+        String serverAddress = args[0];
+        IkhsanTicTacToeClient client = new IkhsanTicTacToeClient(serverAddress);
+        client.play(args[1]);
+    }
+}
